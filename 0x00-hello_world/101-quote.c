352cac099b7b4a24f44f14127cffaf297a098f02
Michael Effiom Effiom <mikeleffiom@gmail.com>
2023-02-16 15:22:06 -0800

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,8 +1,9 @@
+#include <stdio.h>
 #include <unistd.h>
 
 /**
- * main - prints "and that piece of art is useful" - Dora Korpar, 2015-10-19"
- * followed by a newline, to standard error.
+ * main - A c program that prints a line to the standard error
+ * Return: Always 1
  */
 int main(void)
 {
