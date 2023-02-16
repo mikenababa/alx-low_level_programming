0e335ac87e8e95b38431284b73919a664710f01f
Michael Effiom Effiom <mikeleffiom@gmail.com>
2023-02-16 07:01:53 -0800

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 /**
- * maim - A program that prints a line with puts function
+ * main - A program that prints a line with puts function
  * Return: 0 (Sucess)
  */
 int main(void)
