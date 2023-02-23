d1d14d391fee3ef9b96b4415fd22b5cdfec12673
Michael Effiom Effiom <mikeleffiom@gmail.com>
2023-02-22 23:50:46 -0800

    Multiple mains

---
+++ 0x03-debugging/positive_or_negative.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * main - Entry point
+ *
+ * positive_or_negative - prints if integer is positive or negative
+ *
+ * Return - Always 0 (Success)
+ */
+void positive_or_negative(int i)
+{
+	if (i == 0)
+	{
+		printf ("%d is zero\n", i);
+	}
+}
