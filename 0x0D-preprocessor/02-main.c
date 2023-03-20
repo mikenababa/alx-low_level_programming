d1be1c861b7d69f2ca961d90acde28c85194e8e0
Michael Effiom Effiom <mikeleffiom@gmail.com>
2023-03-20 03:39:00 -0700

    File name

---
+++ 0x0D-preprocessor/02-main.c
@@ -0,0 +1,14 @@
+/* A program that prints the name of the file it was compiled from */
+#include <stdio.h>
+
+/**
+ * main - Entry point of the program
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	printf("%s\n", __FILE__);
+
+	return (0);
+}
