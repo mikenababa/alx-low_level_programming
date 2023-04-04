8b6435280f4a30f1eace7afa8a9ae49d790b54a8
Michael Effiom Effiom <mikeleffiom@gmail.com>
2023-04-04 06:37:37 -0700

    Free

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -1,28 +1,27 @@
 #include "lists.h"
 
 /**
-*free_listint2 - frees a linked list
+*free_listint2 - frees a listint_ list
 *@head: pointer to the head of the list
-*
-*Return: void
-**/
+*Return: the head to NULL.
+*/
 
 void free_listint2(listint_t **head)
 {
-listint_t *cursor; /* Declare a pointer to traverse the list */
-listint_t **temp = head; /* Declare a Temp pointer to the head */
+listint_t *tmp; /* Declare a Temp pointer to the head */
 
-if (temp != NULL)
+if (head == NULL)
+return;
 {
 /* Traverse the list and free each node */
 while (*head != NULL)
 {
-cursor = *head;
-free(cursor); /* free the node */
-*head = (*head)->next; /* Move head to the next node */
+tmp = (*head)->next;
+free(*head); /* free the node */
+*head = tmp; /* Move head to the next node */
 }
 
-*temp = NULL; /* ste the head to NULL */
+head  = NULL; /* set the head to NULL */
 
 }
 }
