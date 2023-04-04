54385af707ea101adb49db37513eff436586dad5
Michael Effiom Effiom <mikeleffiom@gmail.com>
2023-04-04 06:01:54 -0700

    Free list

--- 0x13-more_singly_linked_lists/4-free_listint.c
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -2,16 +2,18 @@
 
 /**
  * free_listint - frees a listint_t list.
- * @head: pointer to the list.
- **/
+ * @head: pointer of the list
+ * Return: Void
+ */
+
 void free_listint(listint_t *head)
 {
-	listint_t *tmp;
+	listint_t *tmp; /* Declare a temp pointer */
 
 	while (head)
 	{
-		tmp = head->next;
-		free(head);
-		head = tmp;
+		temp = head; /* store current node in temp */
+		head = head->next; /* update header pointer to the next node */
+		free(temp); /* free memory alloted for the current node */
 	}
 }
