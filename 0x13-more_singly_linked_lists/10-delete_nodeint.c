653e45330be6a260d27c8604c9a977e3a3ee99fd
Michael Effiom Effiom <mikeleffiom@gmail.com>
2023-04-06 04:58:52 -0700

    Delete at index

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -9,22 +9,30 @@
  */
 int delete_nodeint_at_index(listint_t **head, unsigned int index)
 {
+/* Pointers declaration */
 listint_t *prev_node = *head;
 listint_t *current_node;
+/* Integer declaration */
 unsigned int i;
-
+/* If head pointer or node is NULL */
 if (head == NULL || *head == NULL)
 {
 return (-1);
 }
+/* if index is 0, delete the first node */
 if (index == 0)
 {
+/* head pointer points to second node */
 *head = (*head)->next;
+/* Free the original memory */
 free(prev_node);
+/* Return 1 to indicate success */
 return (1);
 }
+/* loctae the node to be deleted */
 for (i = 1; i < index; i++)
 {
+/* Return -1 to indicate failure */
 if (prev_node == NULL)
 {
 return (-1);
